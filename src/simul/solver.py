be34b30ff034185633d992ce30c4fbb35cd401b9
Danylo Borysenko <danborys@c3r3s6.42prague.com>
2026-05-29 17:19:26 +0200

    zone_priorities were added to solver

--- src/simul/solver.py
+++ src/simul/solver.py
@@ -1,5 +1,6 @@
 from typing import Dict, Tuple
-from .models import Hub, Connection
+from .models import Hub, Connection, SimulationMap
+from src.domain import ZoneType
 
 
 class ReservationMap:
@@ -22,9 +23,16 @@ class ReservationMap:
 
 
 class Solver:
-    def __init__(self):
+    def __init__(self, map: SimulationMap) -> None:
         self.heuristics = self._calculate_heuristics()
+        self.map = map
+        self.zone_priorities = {
+            ZoneType.NORMAL: 1.0,
+            ZoneType.RESTRICTED: 2.0,
+            ZoneType.PRIORITY: 0.9
+        }
 
     def _calculate_heuristics(self) -> Dict[str, float]:
-
+        heuristics = {self.map.end_hub: 0.0}
+        
         return {}
